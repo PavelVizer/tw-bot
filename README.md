3f78ee5d527cf3ab5a46e0a81a9085d2dddf5f2f
Pavlo Vizer <weiserpv@gmail.com>
2024-03-26 22:40:51 +0200

    added initial response and added readme

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# tw-bot
+# TW-Bot Bot
+
+Welcome to the TW-Bot Telegram bot! This bot is designed to assist users with a variety of tasks and provide information on demand.
+
+## Features
+
+As of now, the bot supports the following commands:
+•  `/help` - Display the help information about the bot and its features.
+
+•  `/version` - Show the current version of the bot.
+
+•  `/start` - Start interacting with the bot.
+
+
+## Getting Started
+
+To start using the TW-Bot bot, simply click on the link: [TW-Bot Bot](https://t.me/TW-Bot_bot) and hit the `/start` command.
+
+## More to Come
+
+We are actively developing the bot and will be adding more features soon. Stay tuned!
+
+## Feedback
+
+Your feedback is important to us. If you have any suggestions or issues, please reach out.
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE) file for details.
--- cmd/twbot.go
+++ cmd/twbot.go
@@ -1,19 +1,28 @@
 /*
-Copyright © 2024 NAME HERE <EMAIL ADDRESS>
-
+Copyright © 2024 Pavlo Vizer <somemail@somedomain.net>
 */
 package cmd
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"time"
 
 	"github.com/spf13/cobra"
+	telebot "gopkg.in/telebot.v3"
+)
+
+var (
+	//TeleToken bot
+	TeleToken = os.Getenv("TELE_TOKEN")
 )
 
 // twbotCmd represents the twbot command
 var twbotCmd = &cobra.Command{
-	Use:   "twbot",
-	Short: "A brief description of your command",
+	Use:     "twbot",
+	Aliases: []string{"start"},
+	Short:   "A brief description of your command",
 	Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
 
@@ -21,7 +30,32 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("twbot called")
+		fmt.Printf("twbot %s started", appVersion)
+		twbot, err := telebot.NewBot(telebot.Settings{
+			URL:    "",
+			Token:  TeleToken,
+			Poller: &telebot.LongPoller{Timeout: 10 * time.Second},
+		})
+
+		if err != nil {
+			log.Fatalf("Please check TELE_TOKEN env variable %s", err)
+			return
+		}
+
+		twbot.Handle(telebot.OnText, func(m telebot.Context) error {
+
+			log.Print(m.Message().Payload, m.Text())
+			payload := m.Message().Payload
+
+			switch payload {
+			case "hello":
+				err = m.Send(fmt.Sprintf("Hello I'm TW-bot %s", appVersion))
+			}
+
+			return err
+		})
+
+		twbot.Start()
 	},
 }
 
